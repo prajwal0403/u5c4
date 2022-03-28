adce42eb58301a20bb2e39097c63f7fcb3c6e334
Prajwal Raut <rautprajwal546@gmail.com>
2022-03-28 14:27:48 +0530

    ok

---
+++ eval/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routers } from './Components/Routers/Routers';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Routers /> */}
+      <h3>bas kua</h3>
+    </div>
+  );
+}
+
+export default App;
