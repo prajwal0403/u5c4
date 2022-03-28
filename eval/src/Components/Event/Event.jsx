63446ced165b7b2d1ad2303896cf5640b33a096e
Prajwal Raut <rautprajwal546@gmail.com>
2022-03-28 16:57:07 +0530

    okk

--- eval/src/Components/Event/Event.jsx
+++ eval/src/Components/Event/Event.jsx
@@ -8,16 +8,20 @@ export const Event = () => {
   const [data,setData]=useState({});
   const {id}=useParams();
   useEffect(()=>{
-    axios.get(`http://localhost:8080/meetup/${id}`).then((res)=>{
+    axios.get(`http://localhost:8080/meetups/${id}`).then((res)=>{
       setData(res.data)
-      console.log(res.data);
     })
   },[])
 
   return (
     <div className="eventContainer">
-      <h1>{data.title}</h1>
-     
+       <div className="title"><h1>{data.title}</h1></div>
+                  <div className="theme">{data.theme}</div>
+                  <div className="description">{data.description}</div>
+                  <div className="date">{data.date}</div>
+                  <div className="time">{data.time}</div>
+                  <div className="location">{data.location}</div>
+                  <div className="image"><img height="250px" width="250px" src={data.image} alt="" /></div> 
       {/* add your children here (divs)
       ex : title, theme, description, date, time, location, image(optional)
       the classNames should be also : title, theme, description, date, time, location, image(optional)
